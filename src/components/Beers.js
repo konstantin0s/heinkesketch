a523dcc77d91011ce3bd4c4cbdf2a6f4c9cf7eed
Constantin Tofan <you@example.com>
2020-02-02 20:26:41 +0100

    Add a little of style on cards

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -30,6 +30,13 @@ class Beers extends Component {
 
             <div className="cardo">
   <img className="card-img-top" src={beer.image_url} alt="Card image cap" />
+
+  <svg className="card__svg" viewBox="0 0 800 500">
+
+<path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
+<path className="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"/>
+</svg>
+
   <div className="card-body">
     <h5 className="card-title">{beer.name}</h5>
     <p className="card-text">{beer.description.slice(50, 100)}</p>
